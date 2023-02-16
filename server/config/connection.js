ec44c7575835c7731362cf5350b65607eb1d3960
LRicciardo <liane.ricciardo@gmail.com>
2023-02-15 19:55:46 -0600

    temp URI connection Change

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI ||
-   'mongodb://127.0.0.1:27017/mernshopping', 
-// "mongodb+srv://LRicciardo:MongoPass123@cluster0.nrfy0d9.mongodb.net/mernshopping?retryWrites=true&w=majority",
+  //  'mongodb://127.0.0.1:27017/mernshopping', 
+"mongodb+srv://LRicciardo:MongoPass123@cluster0.nrfy0d9.mongodb.net/mernshopping?retryWrites=true&w=majority",
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
